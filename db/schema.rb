d551d5bc813357c22c2965281c8cb0aa56c5ffb4
Rana-Mudassir <mduasire38@gmail.com>
2023-07-01 02:15:18 +0500

    Fix data models

--- db/schema.rb
+++ db/schema.rb
@@ -16,21 +16,21 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_30_063052) do
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
-    t.bigint "author_id_id"
-    t.bigint "post_id_id"
+    t.bigint "author_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id_id"], name: "index_comments_on_author_id_id"
-    t.index ["post_id_id"], name: "index_comments_on_post_id_id"
+    t.index ["author_id"], name: "index_comments_on_author_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "author_id_id"
-    t.bigint "post_id_id"
+    t.bigint "author_id"
+    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id_id"], name: "index_likes_on_author_id_id"
-    t.index ["post_id_id"], name: "index_likes_on_post_id_id"
+    t.index ["author_id"], name: "index_likes_on_author_id"
+    t.index ["post_id"], name: "index_likes_on_post_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -38,10 +38,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_30_063052) do
     t.text "text"
     t.integer "comments_counter", default: 0
     t.integer "likes_counter", default: 0
-    t.bigint "author_id_id"
+    t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["author_id_id"], name: "index_posts_on_author_id_id"
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -53,9 +53,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_30_063052) do
     t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "comments", "posts", column: "post_id_id"
-  add_foreign_key "comments", "users", column: "author_id_id"
-  add_foreign_key "likes", "posts", column: "post_id_id"
-  add_foreign_key "likes", "users", column: "author_id_id"
-  add_foreign_key "posts", "users", column: "author_id_id"
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
